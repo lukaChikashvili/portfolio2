54953de2f2d2f5bddc1fa28d1d57646232e6891f
luka chikashvili <lukachikashvili2020@gmail.com>
2024-08-09 19:15:47 +0400

    add car 3d model

--- portfolio2/src/components/CanvasElement.jsx
+++ portfolio2/src/components/CanvasElement.jsx
@@ -5,7 +5,7 @@ import { OrbitControls } from '@react-three/drei'
 import Lights from './Lights'
 const CanvasElement = () => {
   return (
-    <Canvas camera={ { fov: 75, near: 0.2, far: 1000, position: [4, -1, 5] } }>
+    <Canvas camera={ { fov: 75, near: 0.2, far: 1000, position: [4, -0.8, 5] } }>
         <OrbitControls makeDefault />
         <Lights />
      <Home />
--- portfolio2/src/components/Home.jsx
+++ portfolio2/src/components/Home.jsx
@@ -4,30 +4,32 @@
 
 import { useGLTF, useMatcapTexture } from '@react-three/drei'
 import React, { useEffect } from 'react'
+import { MeshMatcapMaterial } from 'three';
 
 
 const Home = () => {
 
 // use matcap material
-const [ texture ] = useMatcapTexture('637598_B7C4D3_22293A_9BACBF', 256);
+const [ texture ] = useMatcapTexture('AB2D08_C35E5A_C75522_C56C70', 256);
+const [ texture2 ] = useMatcapTexture('D8C949_F5F19E_6B7855_9A9858', 256);
 
-// street light 3d model
-const streetLight = useGLTF('./street_light.glb');
-
+// car 3d model
+const car = useGLTF('./car.glb');
 
      
   return (
      <>
      
-     <mesh position = {[ 0, -2, 0 ]} rotation = {[ -Math.PI * 0.5, 0, -0.8 ]}>
+     <mesh receiveShadow position = {[ 0, -2, 0 ]} rotation = {[ -Math.PI * 0.5, 0, -0.8 ]}>
        <planeGeometry args={[ 150, 10 ]}/>
        <meshMatcapMaterial matcap={texture} />
 
      </mesh>
 
-     <primitive object={streetLight.scene} scale = {0.06} position = {[ 0, -2, 0 ]} />
- 
- 
+    <primitive castshadow object={car.scene} rotation = {[ 0, -2.45, 0 ]}  position = {[0.8, -2, 1 ]} />
+
+
+   
 
     
      </>

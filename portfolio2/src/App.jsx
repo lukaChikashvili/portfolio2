6a162cbf265ae0e1c22bcf583078b1f8adb88439
luka chikashvili <lukachikashvili2020@gmail.com>
2024-08-09 22:09:08 +0400

    removed everything

--- portfolio2/src/App.jsx
+++ portfolio2/src/App.jsx
@@ -7,7 +7,7 @@ import { UserContext } from "./context/UserContext"
 function App() {
  
   // retrieve state from context
-  const { setFirstPage, setSecondPage, setThirdPage} = useContext(UserContext);
+  const { setFirstPage, setSecondPage, setThirdPage, firstPage} = useContext(UserContext);
 
 
   return (
@@ -18,14 +18,8 @@ function App() {
     
      </Routes>
 
-     <div className="absolute bg-transparent top-0 left-0 w-full min-h-screen z-10">
-         <div className="absolute  right-12 top-56 flex flex-col gap-4   ">
-            <button onClick={() => setFirstPage(true)}  className="cursor-pointer pointer-events-auto ">001</button>
-            <button onClick={() => setSecondPage(true)} className="cursor-pointer pointer-events-auto">002</button>
-            <button onClick={() => setThirdPage(true)}  className="cursor-pointer pointer-events-auto">003</button>
-            <button>004</button>
-         </div>
-       </div>
+     
+       
     </>
   )
 }
--- portfolio2/src/context/UserContext.jsx
+++ portfolio2/src/context/UserContext.jsx
@@ -7,15 +7,10 @@ export const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
   
-    // first page
-    const [firstPage, setFirstPage] = useState(false);
-    // second page
-    const [secondPage, setSecondPage] = useState(false);
-    // third page
-    const [thirdPage, setThirdPage] = useState(false);
+    
 
     return (
-        <UserContext.Provider value = {{ firstPage, setFirstPage, secondPage, setSecondPage, thirdPage, setThirdPage }}>
+        <UserContext.Provider value = {{ }}>
           {children}
         </UserContext.Provider>
     )

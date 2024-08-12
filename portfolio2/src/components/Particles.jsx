beababf3ce96109e287a9c089f42e7c24490eaa7
luka chikashvili <lukachikashvili2020@gmail.com>
2024-08-12 15:30:35 +0400

    add 2d canvas

--- portfolio2/src/components/Particles.jsx
+++ portfolio2/src/components/Particles.jsx
@@ -8,6 +8,29 @@ import { useTexture } from '@react-three/drei'
 
 const Particles = () => {
 
+
+    // 2d canvas
+    const canvas = document.createElement('canvas');
+    canvas.width = 128;
+    canvas.height = 128;
+    canvas.style.position = "fixed";
+    canvas.style.width = "512px";
+    canvas.style.height = "512px";
+    canvas.style.top = 0;
+    canvas.style.left = 0;
+    canvas.style.zIndex = 10;
+    document.body.append(canvas);
+
+    // context
+
+   const context = canvas.getContext('2d');
+   context.fillRect(0, 0, canvas.width, canvas.height);
+  
+   
+  
+
+
+
   const texture = useTexture('./images.jpg');
    
     const particleRef = useRef();

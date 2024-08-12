d26b89be7ef32ce55173ee9d410f8a80637a903f
luka chikashvili <lukachikashvili2020@gmail.com>
2024-08-12 16:34:01 +0400

    add cursor particle animation

--- portfolio2/src/components/Particles.jsx
+++ portfolio2/src/components/Particles.jsx
@@ -14,13 +14,8 @@ const Particles = () => {
     const canvas = document.createElement('canvas');
     canvas.width = 128;
     canvas.height = 128;
-    canvas.style.position = "fixed";
-    canvas.style.width = "512px";
-    canvas.style.height = "512px";
-    canvas.style.top = 0;
-    canvas.style.left = 0;
-    canvas.style.zIndex = 10;
-    document.body.append(canvas);
+   
+    
 
     // context
 
@@ -30,7 +25,7 @@ const Particles = () => {
    const glowImage = new Image();
    glowImage.src = './circle.png';  
 
-
+   const displacementTexture = new THREE.CanvasTexture(canvas);
 
   const texture = useTexture('./images.jpg');
    
@@ -38,11 +33,25 @@ const Particles = () => {
 
     const particleGeometry = new THREE.PlaneGeometry(10, 10, 128, 128);
 
+     const intensityArr = new Float32Array(particleGeometry.attributes.position.count);
+     const anglesArr = new Float32Array(particleGeometry.attributes.position.count);
+
+     for(let i = 0; i < particleGeometry.attributes.position.count; i++) {
+
+       intensityArr[i] = Math.random();
+       anglesArr[i] = Math.random() * Math.PI * 2;
+     }
+
+     particleGeometry.setAttribute('aIntensity', new THREE.BufferAttribute(intensityArr, 1));
+     particleGeometry.setAttribute('aAngle', new THREE.BufferAttribute(anglesArr, 1));
+
+
     const particleMaterial = new THREE.ShaderMaterial({
        vertexShader: vertex,
        fragmentShader: fragment,
        uniforms: {
-        uPictureTexture: new THREE.Uniform(texture)
+        uPictureTexture: new THREE.Uniform(texture),
+        uDisplacementTexture: new THREE.Uniform(displacementTexture)
        }
     });
 
@@ -59,7 +68,7 @@ const Particles = () => {
       });
 
 
-      const displacementTexture = new THREE.CanvasTexture(canvas);
+     
 
       // interactive plane ref
       let interactiveRef = useRef();
@@ -82,7 +91,12 @@ const Particles = () => {
             
         }
     }
-       context.globalCompositeOperation = "lighten";
+    context.globalCompositeOperation = 'source-over';
+    context.globalAlpha = 0.02;
+     context.fillRect(0, 0, canvas.width, canvas.height);
+
+       context.globalCompositeOperation = 'lighten'
+       context.globalAlpha = 1;
         context.drawImage(
           glowImage, 
           canvasCursor.x,
@@ -103,7 +117,7 @@ const Particles = () => {
      <primitive ref = {particleRef} object={new THREE.Points(particleGeometry, particleMaterial)} rotation = {[ 0, 0.7, 0 ]} />
      <mesh rotation = {[ 0, 0.7, 0 ]} ref={interactiveRef}>
        <planeGeometry args = {[10, 10]} />
-       <meshBasicMaterial color = "red" />
+       <meshBasicMaterial visible = {false}/>
      </mesh>
    </>
   )
--- portfolio2/src/shaders/particle/vertex.glsl
+++ portfolio2/src/shaders/particle/vertex.glsl
@@ -1,11 +1,33 @@
 uniform sampler2D uPictureTexture;
 varying vec3 vColor;
+uniform sampler2D uDisplacementTexture;
+attribute float aIntensity;
+attribute float aAngle;
+
 void main()
 {
 
+// displacement
+vec3 newPosition = position;
+
+float displacementIntensity = texture(uDisplacementTexture, uv).r; 
+displacementIntensity = smoothstep(0.1, 1.0, displacementIntensity);
+
+vec3 displacement = vec3(
+    cos(aAngle) * 0.2,
+    sin(aAngle) * 0.2,
+    1.0
+);
+
+displacement = normalize(displacement);
+
+displacement *= displacementIntensity;
+displacement *= 3.0;
+displacement *= aIntensity;
+newPosition += displacement; 
 
     // Final position
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
